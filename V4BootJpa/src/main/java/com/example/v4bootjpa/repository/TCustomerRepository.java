b8255a96740175ce7a318dd5aee090f1b7734013
djordje <przulj@gmail.com>
2018-03-23 18:28:24 +0100

    Dodate repozitorijum.

---
+++ V4BootJpa/src/main/java/com/example/v4bootjpa/repository/TCustomerRepository.java
@@ -0,0 +1,9 @@
+package com.example.v4bootjpa.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.v4bootjpa.model.TCustomer;
+
+public interface TCustomerRepository extends JpaRepository<TCustomer,Integer>{
+
+}
