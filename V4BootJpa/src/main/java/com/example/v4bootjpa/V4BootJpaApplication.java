5e9b9da5fc3c4b1de3d212b3316a994b142b70b6
djordje <przulj@gmail.com>
2018-03-23 17:59:42 +0100

    Spring Boot 2.0 projekat.

---
+++ V4BootJpa/src/main/java/com/example/v4bootjpa/V4BootJpaApplication.java
@@ -0,0 +1,12 @@
+package com.example.v4bootjpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class V4BootJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(V4BootJpaApplication.class, args);
+	}
+}
---
+++ V4BootJpa/src/test/java/com/example/v4bootjpa/V4BootJpaApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.v4bootjpa;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class V4BootJpaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
