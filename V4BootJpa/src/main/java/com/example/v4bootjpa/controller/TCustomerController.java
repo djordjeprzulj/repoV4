c44ecf75474908d9cb0cff8e0f219d864ebfecaf
djordje <przulj@gmail.com>
2018-03-23 18:32:53 +0100

    Dodata metoda readAll u REST kontroleru.

--- V4BootJpa/src/main/java/com/example/v4bootjpa/controller/TCustomerController.java
+++ V4BootJpa/src/main/java/com/example/v4bootjpa/controller/TCustomerController.java
@@ -1,8 +1,12 @@
 package com.example.v4bootjpa.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.v4bootjpa.model.TCustomer;
 import com.example.v4bootjpa.repository.TCustomerRepository;
 
 @RestController
@@ -10,4 +14,9 @@ public class TCustomerController {
 
 	@Autowired
 	TCustomerRepository tCustomerRepository;
+	
+	@RequestMapping("/allCustomers")
+	public List<TCustomer> readAll() {
+		return tCustomerRepository.findAll();
+	}
 }
